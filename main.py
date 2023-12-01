ba6987d206fafdc38b0cf509a65127ec0f1e3138
Amy <amy@data-nz.com>
2023-12-01 17:12:04 +1300

    AWS Re/Start Course, November 2023

---
+++ main.py
@@ -0,0 +1,25 @@
+rom fastapi import FastAPI
+from typing import Union 
+# You can use Union to declare that a parameter can be one of several types. 
+# List, Dict, Tuple, Set, etc. are all available in the typing module.
+from pydantic import BaseModel
+
+app = FastAPI()
+
+#my Inventory Items Dictonary
+
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, item: Item, q: Union[str, None] = None): #Default value for q is None
+    return {"item_id": item_id, "q": q, "item": item}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
